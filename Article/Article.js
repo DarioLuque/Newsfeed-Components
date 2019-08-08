5d37a9e5e33f2afd433d700d0eb08c3b43b4f94f
dario luque <dario.lu92@gmail.cm>
2019-08-08 11:40:08 -0700

    fixed bug with new paragraph

--- Article/Article.js
+++ Article/Article.js
@@ -71,13 +71,29 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Jedi Web Development in 2019',
+    title: 'Professional Software Development in 2019',
     date: 'Jan 1st, 2019',
-    firstParagraph: `Lucas ipsum dolor sit amet darth darth sidious sebulba dooku jinn jawa jade mothma wampa. Obi-wan kashyyyk darth solo. Solo organa twi'lek baba padmé sith biggs leia hoth. Skywalker ben lobot zabrak aayla jawa alderaan. Vader solo moff skywalker secura darth han mon. Zabrak dantooine yavin grievous sith organa antilles sidious darth. Amidala c-3po gamorrean fisto organa skywalker skywalker ewok mara. Antilles hutt wampa naboo antilles. Yoda yoda solo skywalker. Calrissian thrawn anakin skywalker mon vader amidala hutt jinn. `,
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+      hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+      Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
 
-    secondParagraph: `Lobot mon jar grievous palpatine mon wedge organa maul. Antilles tatooine moff endor fett bespin organa. Calamari sidious kit luuke darth darth twi'lek chewbacca. Moff grievous skywalker moff. Grievous darth owen dooku han darth kenobi darth mon. Moff fisto biggs darth jabba darth palpatine gonk. Antilles mara coruscant lars dantooine luke. Tatooine yoda kashyyyk kenobi hutt boba cade. Hutt luuke darth wedge leia boba. Kashyyyk organa organa tatooine anakin skywalker. Kenobi binks ponda hutt sidious maul. `,
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+      hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+      hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+      hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
 
-    thirdParagraph: `Windu skywalker darth gonk darth. Solo windu fett amidala darth dantooine. Skywalker organa lobot moff. Watto ventress jade qui-gon coruscant jinn luke. Palpatine moff boba palpatine sidious kenobi kessel hoth lando. Moff r2-d2 dooku ventress moff. Vader owen moff obi-wan. Lars palpatine hutt grievous organa amidala. R2-d2 baba calrissian kessel endor mace darth fett solo. Skywalker jabba moff c-3p0 skywalker cade solo darth. Dooku jango lando skywalker. Hutt ackbar hutt qui-gonn yavin. Baba twi'lek sith jinn anakin.`
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+      Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+      Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Javascript Software Development in 2019',
+    date: 'Jan 1st, 2019',
+    firstParagraph: `Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De        apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.`,
+
+    secondParagraph: `Lucio fulci tremor est dark vivos magna. Expansis creepy arm yof darkness ulnis witchcraft missing carnem armis Kirkman Moore and Adlard caeruleum in locis. Romero     morbo Congress amarus in auras. Nihil horum sagittis tincidunt, zombie slack-jawed gelida survival portenta. The unleashed virus est, et iam zombie mortui ambulabunt super terram. Souless mortuum glassy-eyed oculos attonitos indifferent back zom bieapoc alypse. An hoc dead snow braaaiiiins sociopathic incipere Clairvius Narcisse, an ante? Is bello mundi z?`,
+
+    thirdParagraph: `In Craven omni memoria patriae zombieland clairvius narcisse religionis sunt diri undead historiarum. Golums, zombies unrelenting et Raimi fascinati beheading. Maleficia! Vel cemetery man a modern bursting eyeballs perhsaps morbi. A terrenti flesh contagium. Forsitan deadgurl illud corpse Apocalypsi, vel staggering malum zomby poenae chainsaw zombi horrifying fecimus burial ground. Indeflexus shotgun coup de poudre monstra per plateas currere. Fit de decay nostra carne undead. Poenitentiam violent zom biehig hway agite RE:dead pœnitentiam! Vivens mortua sunt apud nos night of the living dead.`
   }
 ];
 
